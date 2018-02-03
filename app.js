08520b4b5f427b5bc96dca7369228ba33cb55d87
“Julie-S-Bernal“ <Julies.bernal@gmail.coml>
2018-02-03 16:37:37 +0000

    first part of codingtest working

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const fs = require('fs');
+const Walker = require('walker');
+const showdown  = require('showdown')
+
+const app = express();
+const converter = new showdown.Converter()
+const templatePath = process.cwd() + '/template.html'
+
+
+Walker('./content')
+  .on('file', (entry, _) => {
+    pageSettings = entry.split('/')
+    let inMemoryTemplate = '';
+    let htmlFromMd = '';
+    fs.readFile(templatePath, (err, data) => {
+      if (err) throw err; inMemoryTemplate = data.toString();
+    });
+
+    fs.readFile( process.cwd() + '/' + entry, (err, data) => {
+      if (err) throw err; htmlFromMd = converter.makeHtml(data.toString());
+    });
+
+    app.get('/' + pageSettings[1], (req, res) => {  
+      const htmlPage = inMemoryTemplate.replace('{{content}}', htmlFromMd );
+      res.send(htmlPage);
+    });    
+  })
+
+app.listen(3000,  () => {
+  console.log('Example app listening on port 3000!');
+});
